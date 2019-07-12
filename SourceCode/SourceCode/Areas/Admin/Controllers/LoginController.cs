a549c1c5e5b58ebd6a840d63fa3e244df92044fb
ngogiang270593 <dkgiang.a2@gmail.com>
2019-07-12 10:10:00 +0700

    up

--- SourceCode/SourceCode/Areas/Admin/Controllers/LoginController.cs
+++ SourceCode/SourceCode/Areas/Admin/Controllers/LoginController.cs
@@ -4,6 +4,8 @@ using SourceCode.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 namespace SourceCode.Areas.Admin.Controllers
@@ -11,15 +13,18 @@ namespace SourceCode.Areas.Admin.Controllers
     public class LoginController : Controller
     {
         // GET: Admin/Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Login(LoginModel model)
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
         {
             var dao = new UsersDao();
-            var result = dao.Login(model.UserName, model.Password);
+            string Password = EncodePassword(model.Password);
+            var result = dao.Login(model.UserName, Password);
             if (result)
             {
                 var user = dao.GetById(model.UserName);
@@ -27,12 +32,29 @@ namespace SourceCode.Areas.Admin.Controllers
                 userSession.UserName = user.UserName;
                 userSession.UserID = user.UserId;
                 Session.Add(CommonConstants.USER_SESSION, userSession);
+                return RedirectToAction("Index", "Index");
             }
             else
             {
                 ModelState.AddModelError("", "Đăng nhập không đúng.");
             }
             return View();
+        }
+
+        public static string EncodePassword(string originalPassword)
+        {
+            //Declarations
+            Byte[] originalBytes;
+            Byte[] encodedBytes;
+            System.Security.Cryptography.MD5 md5;
+
+            //Instantiate MD5CryptoServiceProvider, get bytes for original password and compute hash (encoded password)
+            md5 = new MD5CryptoServiceProvider();
+            originalBytes = ASCIIEncoding.Default.GetBytes(originalPassword);
+            encodedBytes = md5.ComputeHash(originalBytes);
+
+            //Convert encoded bytes back to a 'readable' string
+            return BitConverter.ToString(encodedBytes).Replace("-", "");
         }
     }
 }
